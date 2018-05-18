a5b5154a8120cf2f59a64e8bb2bed3e566d9a248
rodicabulai <rodica.bulai@ati.utm.md>
2018-05-18 20:43:14 +0300

    al cincilea comiit

---
+++ lesson5/Tekwill_ORADB.sql
@@ -0,0 +1,9 @@
+--select * from all_tables
+--select * from dual
+--select * from EMPLOYEES
+--select * FROM employees
+
+--select distinct FIRST_NAME from employees
+--select * from departments
+--select distinct salary, DEPARTMENT_ID from employees
+describe employees
